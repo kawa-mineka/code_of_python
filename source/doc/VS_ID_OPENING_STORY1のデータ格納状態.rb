b577b15a377cb46f098460db98a6bcf1267a3d30
kawamineka <kawa.mineka+github@gmail.com>
2022-12-31 12:50:38 +0900

    とりあえず、生中ではなくてコミット

--- source/doc/VS_ID_OPENING_STORY1のデータ格納状態.rb
+++
@@ -1,8 +0,0 @@
-VS_ID_OPENING_STORY1のデータ格納状態
-scroll_textの中身はこういう感じで収納される
-
-
-
-[0, 60, 120, 40, [['', 7, 2, 5], ['', 7, 2, 5], ['Augusta Ada King', 7, 2, 5], ['Countess of Lovelace née Byron', 7, 3, 5], ['10 December 1815', 7, 3, 0], ['–--', 7, 3, 0], ['27 November 1852', 7, 4, 0], ['was an English mathematician', 7, 4, 0], ['and writer', 7, 4, 0], ['chiefly known for her work', 7, 4, 0], ["on Charles Babbage's proposed", 7, 2, 0], ['mechanical general-purpose computer', 7, 2, 0], ['the Analytical Engine.', 7, 2, 0], ['She was the first to recognise', 7, 2, 0], ['that the machine', 7, 2, 0], ['had applications beyond', 7, 2, 0], ['pure calculation', 7, 
-2, 0], ['and to have published', 7, 2, 0], ['the first algorithm intended', 7, 2, 0], ['to be carried out by', 7, 2, 0], ['such a machine. As a result', 7, 2, 0], ['she is often regarded', 7, 2, 0], ['as the first computer programmer', 7, 2, 0], ['', 7, 2, 0], ['', 7, 2, 0], ['', 7, 2, 0], 
-['', 7, 2, 0], ['', 7, 2, 0]], [], 0, 0, 0, 160, 16, 1, 5, 8, 8, 8, 8, 0, 0, 0, 0, 22, 2, 0, 0]
